1249bcb7b896e2c659762577e1ba545d146bca99
M. Greyson Christoforo <grey@christoforo.net>
2025-05-20 15:06:50 -0600

    Update makepkg-url.sh

--- makepkg-url.sh
+++ makepkg-url.sh
@@ -2,7 +2,7 @@
 # builds an Arch package from files given in a (curl glob formatted) URL
 # origionally from https://gist.github.com/greyltc/8a93d417a052e00372984ff8ec224703
 # example usage; (re)build and install the aurutils package:
-# bash <(curl -sL https://raw.githubusercontent.com/greyltc/build-arch-packages-action/60fefbbe9d167d0d1b747e456c3158745c66a6ac/makepkg-url.sh) "https://aur.archlinux.org/cgit/aur.git/plain/{PKGBUILD,aurutils.changelog,aurutils.install}?h=aurutils" --install --force
+# bash <(curl -sL https://raw.githubusercontent.com/greyltc/build-arch-packages-action/5a628566eba3688cb8adb2ee3e03e5b7097fabe4/makepkg-url.sh) "https://aur.archlinux.org/cgit/aur.git/plain/{PKGBUILD,aurutils.changelog,aurutils.install}?h=aurutils" --install --force
 
 set -e
 
