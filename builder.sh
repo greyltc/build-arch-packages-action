4550fa0da29597da83f52853e90e5c9ca486fbff
M. Greyson Christoforo <grey@christoforo.net>
2025-05-26 00:24:29 -0600

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -46,7 +46,8 @@ main() {
 	pacman --sync --refresh --sysupgrade --noconfirm
 
 	# bootstrap
- 	runuser -u archie -- makepkg-url "https://aur.archlinux.org/cgit/aur.git/plain/{PKGBUILD,fakeroot.install}?h=fakeroot-tcp" --syncdeps --install --clean --noconfirm --rmdeps
+ 	runuser -u archie -- makepkg-url "https://aur.archlinux.org/cgit/aur.git/plain/{PKGBUILD,fakeroot.install}?h=fakeroot-tcp" --syncdeps --clean --noconfirm --rmdeps
+  	yes | pacman -U /out/cache/custom/pkg/fakeroot*
  	echo "Bootstrapping paru"
 	runuser -u archie -- makepkg-url "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=paru" --syncdeps --install --clean --noconfirm --rmdeps
 	echo "Bootstrapping aurutils"
